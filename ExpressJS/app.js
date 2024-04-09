89eacc35c033e6630521115805479f6c22563a33
Usman Zia <u_zia@uncg.edu>
2024-04-09 18:54:03 -0400

    backend ready

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -20,19 +20,162 @@ const dbConfig = {
   host: 'localhost',
   port: 6000,
   user: 'root',
-  database: 'CSC_471_Prj 4',
+  database: 'Prj5',
   password: '',
 };
 
 const connection = mysql.createConnection(dbConfig);
 
-app.get("/all", (req, res) => {
-    connection.query('SELECT * FROM project', (error, results) => {
+
+/**
+ * READ endpoints
+ */
+
+app.get("/employees", (req, res) => {
+    connection.query('SELECT * FROM employee',
+    (error, results) => {
         if (error) {
             console.error('Error executing query:', error);
             res.status(500).send('Error executing query');
         } else {
             res.json(results);
+        }
+    });
+});
+
+app.get("/dependents", (req, res) => {
+    connection.query('SELECT * FROM dependent',
+    (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(500).send('Error executing query');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+app.post("/employee", (req, res) => {
+    const { ssn } = req.body;
+    connection.query('SELECT * FROM employee WHERE ssn = ?', [ssn],
+    (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(500).send('Error executing query');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+app.post("/dependent", (req, res) => {
+    const { ssn } = req.body;
+    connection.query('SELECT * FROM dependent WHERE ssn = ?', [ssn],
+    (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(500).send('Error executing query');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+
+/**
+ * CREATE endpoints
+ */
+
+app.post("/createEmployee", (req, res) => {
+    const { ssn, Fname, Minit, Lname, dob, address } = req.body;
+    connection.query(`      
+        INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?)`, [ssn, Fname, Minit, Lname, dob, address],
+        (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(500).send('Error executing query');
+        } else {
+            const result = connection.query('SELECT * FROM employee WHERE ssn = ?', [ssn])
+            res.json(result[0]);
+        }
+    });
+});
+
+app.post("/createDependent", (req, res) => {
+    const { ssn, name, relationship } = req.body;
+    connection.query(`      
+        INSERT INTO dependent VALUES (?, ?, ?)`, [ssn, name, relationship],
+        (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(500).send('Error executing query');
+        } else {
+            const result = connection.query('SELECT * FROM dependent WHERE ssn = ?', [ssn])
+            res.json(result[0]);
+        }
+    });
+});
+
+
+/**
+ * UPDATE endpoints
+ */
+
+app.post("/updateEmployee", (req, res) => {
+    const { ssn, Fname, Minit, Lname, dob, address } = req.body;
+    connection.query(`      
+        UPDATE employee SET Fname = ?, Minit = ?, Lname = ?, dob = ?, address = ? WHERE ssn = ?`, [Fname, Minit, Lname, dob, address, ssn],
+        (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(500).send('Error executing query');
+        } else {
+            res.status(200).send('Success');
+        }
+    });
+});
+
+app.post("/updateDependent", (req, res) => {
+    const { ssn, name, relationship } = req.body;
+    connection.query(`      
+        UPDATE dependent SET name = ?, relationship = ? WHERE ssn = ?`, [name, relationship, ssn],
+        (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(500).send('Error executing query');
+        } else {
+            res.status(200).send('Success');
+        }
+    });
+});
+
+
+/**
+ * DELETE endpoints
+ */
+
+app.post("/deleteEmployee", (req, res) => {
+    const { ssn } = req.body;
+    connection.query('DELETE FROM employee WHERE ssn = ?', [ssn],
+    (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(500).send('Error executing query');
+        } else {
+            res.status(200).send('Success');
+        }
+    });
+});
+
+app.post("/deleteDependent", (req, res) => {
+    const { ssn } = req.body;
+    connection.query('DELETE FROM dependent WHERE ssn = ?', [ssn],
+    (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            res.status(500).send('Error executing query');
+        } else {
+            res.status(200).send('Success');
         }
     });
 });
